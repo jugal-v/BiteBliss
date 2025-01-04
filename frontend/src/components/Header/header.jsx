9c6f17ac245e606a387b06dac5424bf616a41eb7
Jugal-vyas-dev <jugalvyas27@gmail.com>
2025-01-04 19:33:46 +0530

    New

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='header'>
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
-        <p>Choode from a diverse menu featuring a delectable array of dishhes crafted with the finest ingrediants and culinary expperties. Our mission is to satisfy your cravings and elevate your dining experience. One delicious meal at a time.</p>
+        <p>Choose from a diverse menu featuring a delectable array of dishhes crafted with the finest ingrediants and culinary expperties. Our mission is to satisfy your cravings and elevate your dining experience. One delicious meal at a time.</p>
         <button>View Menu</button>
       </div>
     </div>
